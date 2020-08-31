a447588c58f7e57a51d594ef01a98f084ee4699e
snguyen112297 <snguyen@fandm.edu>
2020-08-31 13:47:36 -0400

    Initial commit

--- App.js
+++ App.js
@@ -1,21 +1,95 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+import { withNavigation } from 'react-navigation';
+import { registerRootComponent } from 'expo';
 
-export default function App() {
+class HomeScreen extends React.Component{
+  state={
+    email:"",
+    password:""
+  }
+  email = "snguyen@fandm.edu"
+  password = "abcxyz"
+  email_check = false
+  password_check = false
+
+  render(){
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+        <Text style={styles.logo}>Articles</Text>
+        <View style={styles.inputView}>
+          <TextInput 
+            style={styles.inputText}
+            placeholder="Email"
+            placeholderTextColor="#FFFFFF"
+            onChangeText={text=>this.setState({email:text})}/>
+        </View>
+        <View style={styles.inputView}>
+          <TextInput 
+              style={styles.inputText}
+              placeholder="Password"
+              placeholderTextColor="#FFFFFF"
+              onChangeText={text=>this.setState({password:text})}/>
+        </View>
+        <Button title="LOGIN" onPress={()=> this.props.navigation.navigate('Articles List')}/>
+      </View>
+    );
+  }
+}
+
+const Stack = createStackNavigator();
+
+
+function ArticlesList({navigation}) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Articles List</Text>
     </View>
   );
 }
 
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={HomeScreen} />
+        <Stack.Screen name="Articles List" component={ArticlesList}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'black',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  logo: {
+    fontWeight:"bold",
+    fontSize:50,
+    color:"red",
+    marginBottom:40
+  },
+  inputView:{
+    width:"60%",
+    backgroundColor:"blue",
+    borderRadius: 25,
+    height: 50,
+    marginBottom: 20,
+    justifyContent: "center",
+    padding: 20,
+  },
+  inputText:{
+    height:50,
+    color:"white"
+  },
+  loginText:{
+    color:"white"
   },
 });
